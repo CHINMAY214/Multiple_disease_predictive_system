3dac0fcc3c8353f515813197e9ac8c433bfc2b2a
Chinmay Bonde <118678313+CHINMAY214@users.noreply.github.com>
2024-04-11 17:16:33 +0530

    mdps.py

--- mdps.py
+++ mdps.py
@@ -34,6 +34,7 @@ with st.sidebar:
                            'Parkinsons Prediction'],
                           icons=['activity','heart','person'],
                           default_index=0)
+    st.image("title-image")
     st.info("This Application allows you to check the Diabetics , heart-disease, Parkinson's disease")
   
     
