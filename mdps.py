720e965d529d9a0849565869f642d39e8c6b6247
Chinmay Bonde <118678313+CHINMAY214@users.noreply.github.com>
2024-04-11 16:49:57 +0530

    mdps.py

--- mdps.py
+++ mdps.py
@@ -9,6 +9,10 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+st.set_page_config(page_title="Health Assistant",
+                   layout="wide",
+                   page_icon="🧑‍⚕️")
+
 
 # loading the saved models
 
